6db945d4eab286ee8dbf19bd18054f625cf956d5
thesquatchman <bej02c@gmail.com>
2017-11-21 11:26:54 -0700

    edit docs

---
+++ docs/js/squatch.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.navbar-toggle').click(function(){
+		$(this).closest('.navbar').toggleClass('open');
+	});
+	$('.dropdown-toggle').click(function(){
+		$(this).closest('li').toggleClass('open');
+	});//
+});
