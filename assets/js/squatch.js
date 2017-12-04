663bb83b357dc63b943f1ffce3a773ab045c1102
thesquatchman <bej02c@gmail.com>
2017-12-03 21:26:05 -0700

    started converting to flexbox

--- assets/js/squatch.js
+++ assets/js/squatch.js
@@ -5,4 +5,11 @@ $(document).ready(function(){
 	$('.dropdown-toggle').click(function(){
 		$(this).closest('li').toggleClass('open');
 	});//
+	$( '[data-toggle]' ).click(function(e) {
+		e.preventDefault();
+		var classToToggle = $(this).attr('data-toggle'),
+				elToToggle = $(this).attr('data-target');
+		$(this).toggleClass('active');		
+		$(elToToggle).toggleClass(classToToggle);	
+	});
 });
