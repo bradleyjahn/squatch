d2b69a7b457467928c4b24805bc540b54b28fdf1
thesquatchman <bej02c@gmail.com>
2018-03-15 14:41:23 -0600

    fix readme rename import

--- README.md
+++ README.md
@@ -1,23 +1,31 @@
 # Welcome to Squatch
-Squatch is a flexbox framework for a small to meduim sized site. The goal is to be able to take care of the responsive layout of the site with easy to read and remember css classes, while not being too opinionated about styling components. 
+
+Squatch is a flexbox framework for a small to meduim sized site. The goal is to be able to take care of the responsive layout of the site with easy to read and remember css classes, while not being too opinionated about styling components.
 
 ### to start a new project with gulp
-clone 
-`$ git clone https://github.com/thesquatchman/squatch-sass.git` 
-    
+
+clone
+
+    $ git clone https://github.com/thesquatchman/squatch-sass.git
+
 then run
-`$ npm install`
+
+    $ npm install
 
 watch
-`$ gulp`
+
+    $ gulp
 
 ### to import into an existing project
+
 install
-`$ npm install squatch --save`
+
+    $ npm install squatch --save
 
 @import into your scss with something like:
-`@import "../node_modules/squatch/stylesheets/squatch"`
 
-note: this assumes you are importing from a .scss file in your project root. add some extra "../../" as needed ;)
+    @import "../node_modules/squatch/stylesheets/import"
+
+note: this assumes you are importing from a .scss file in your project-root/stylesheets. add some more/less "../../" as needed ;)
 
 for examples visit http://squatch.co
--- scripts/squatch.js
+++ scripts/squatch.js
@@ -1,72 +1,81 @@
 // This function is called immediately. The second function is passed in
 // as the factory parameter to this function.
-(function (factory) {
+(function(factory) {
   // If there is a variable named module and it has an exports property,
   // then we're working in a Node-like environment. Use require to load
   // the jQuery object that the module system is using and pass it in.
-  if(typeof module === "object" && typeof module.exports === "object") {
-    factory(require("jquery"), window, document);
-  }
-  // Otherwise, we're working in a browser, so just pass in the global
-  // jQuery object.
-  else {
+  if (typeof module === 'object' && typeof module.exports === 'object') {
+    factory(require('jquery'), window, document);
+  } else {
+    // Otherwise, we're working in a browser, so just pass in the global
+    // jQuery object.
     factory(jQuery, window, document);
   }
-}(function($, window, document, undefined) {
-// This code will receive whatever jQuery object was passed in 
-    $.squatch = function(element, options) {
-        var defaults = {
-            activeClass: 'active',
-            toggleAttr: 'data-toggle',
-            targetAttr: 'data-target',
-            onFinished: function() {}
-        }
-        var plugin = this;
-        plugin.settings = {}
+})(function($, window, document, undefined) {
+  // This code will receive whatever jQuery object was passed in
+  $.squatch = function(element, options) {
+    // set default classes and toggle attr, can be changed if in conflict with another plugin
+    var defaults = {
+      activeClass: 'active',
+      toggleAttr: 'data-toggle',
+      targetAttr: 'data-target',
+      onFinished: function() {}
+    };
+    var plugin = this;
+    plugin.settings = {};
 
-        var $element = $(element),  
-             element = element;  
-        plugin.init = function() {
-            plugin.settings = $.extend({}, defaults, options);
-            $element.click(function(e){
-                e.preventDefault();
-                var classToToggle = $element.attr(plugin.settings.toggleAttr),
-                    elToToggle = $element.attr(plugin.settings.targetAttr);
-                    $element.toggleClass(plugin.settings.activeClass);
-                if (elToToggle) {       
-                    $(elToToggle).toggleClass(classToToggle);   
-                } 
-                else {    
-                    var exList = $element.closest('ul'),
-                            elToToggle = exList ? exList.attr(plugin.settings.targetAttr) : false;
-                    if (exList && elToToggle) {
-                        var els = exList.find('[' + plugin.settings.toggleAttr + ']'),
-                                classes = '';
-                        els.each(function(i,e){
-                            $(e).removeClass(plugin.settings.activeClass);
-                            classes = classes + ' ' + $(e).attr(plugin.settings.toggleAttr);
-                        })
-                        $element.addClass(plugin.settings.activeClass);
-                        $(elToToggle).removeClass(classes).addClass(classToToggle);
-                    } else if (classToToggle.indexOf('#') >= 0) {   
-                      $element.siblings().removeClass(plugin.settings.activeClass);         
-                        $(classToToggle).addClass(plugin.settings.activeClass).siblings().removeClass(plugin.settings.activeClass);
-                    } else {
-                        $element.closest('li').toggleClass(classToToggle);
-                    }
-                 }
-                 plugin.settings.onFinished();
+    var $element = $(element),
+      element = element;
+    plugin.init = function() {
+      plugin.settings = $.extend({}, defaults, options);
+      $element.click(function(e) {
+        e.preventDefault();
+        var classToToggle = $element.attr(plugin.settings.toggleAttr),
+          elToToggle = $element.attr(plugin.settings.targetAttr);
+        $element.toggleClass(plugin.settings.activeClass);
+        if (elToToggle) {
+          // both target and toggle class are defined
+          $(elToToggle).toggleClass(classToToggle);
+        } else {
+          // no target is found, try to get target from parent ul
+          var exList = $element.closest('ul'),
+            elToToggle = exList ? exList.attr(plugin.settings.targetAttr) : false;
+          if (exList && elToToggle) {
+            // found parent ul's target, so we're in one at a time list mode
+            var els = exList.find('[' + plugin.settings.toggleAttr + ']'),
+              classes = '';
+            els.each(function(i, e) {
+              $(e).removeClass(plugin.settings.activeClass);
+              classes = classes + ' ' + $(e).attr(plugin.settings.toggleAttr);
             });
+            $element.addClass(plugin.settings.activeClass);
+            $(elToToggle)
+              .removeClass(classes)
+              .addClass(classToToggle);
+          } else if (classToToggle.indexOf('#') >= 0) {
+            // data-toggle is an id, so we're doing tabs
+            $element.siblings().removeClass(plugin.settings.activeClass);
+            $(classToToggle)
+              .addClass(plugin.settings.activeClass)
+              .siblings()
+              .removeClass(plugin.settings.activeClass);
+          } else {
+            // no target so we're opening a dropdown menu
+            $element.closest('li').toggleClass(classToToggle);
+          }
         }
-        plugin.init();
-    }
+        plugin.settings.onFinished(); // optional callback
+      });
+    };
+    plugin.init();
+  };
 
-    $.fn.squatch = function(options) {
-        return this.each(function() {
-            if (undefined == $(this).data('squatch')) {
-                var plugin = new $.squatch(this, options);
-                $(this).data('squatch', plugin);
-            }
-        });
-    }
-}));
+  $.fn.squatch = function(options) {
+    return this.each(function() {
+      if (undefined == $(this).data('squatch')) {
+        var plugin = new $.squatch(this, options);
+        $(this).data('squatch', plugin);
+      }
+    });
+  };
+});
